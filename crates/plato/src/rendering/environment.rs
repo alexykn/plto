3ecb7730853bb37144e92e16c3e49a9c30f2a7b0
alexykn <alexander.knott@posteo.de>
2026-07-29 10:46:36 +0200

    fix: retain newline at eof when rendering templates
    Signed-off-by: alexykn <alexander.knott@posteo.de>

--- crates/plato/src/rendering/environment.rs
+++ crates/plato/src/rendering/environment.rs
@@ -5,6 +5,7 @@ use super::filters;
 pub(crate) fn new_template_environment() -> Environment<'static> {
     let mut env = Environment::new();
     env.set_undefined_behavior(UndefinedBehavior::Strict);
+    env.set_keep_trailing_newline(true);
     filters::register_all(&mut env);
     env
 }
